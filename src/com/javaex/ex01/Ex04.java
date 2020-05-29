b8994f5c2d42ec52528d494531f1e3a483373a6a
hi <hi@DESKTOP-53P5LLN>
2020-05-29 12:39:02 +0900

    데이터통신. Reader, BufferedReader를 사용하여 외부의 데이터를 읽어오자.

---
+++ src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,28 @@
+package com.javaex.ex01;
+
+//데이터통신. Reader, BufferedReader를 사용하여 외부의 데이터를 읽어오자.
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class Ex04 {
+
+	public static void main(String[] args)throws IOException {
+		
+		Reader fr = new FileReader("C:\\javaStudy\\file\\song.text");
+		BufferedReader br = new BufferedReader(fr);
+		
+		while(true) {
+			String str = br.readLine();
+			if(str == null) {
+				break;
+			}
+			System.out.println(str);
+		}
+		
+		br.close();
+		
+	}
+}
